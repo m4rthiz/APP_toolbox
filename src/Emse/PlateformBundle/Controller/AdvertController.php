d2f59d9b2a31e749f158f27e0f010c6d524be41a
marie.thizy <marie.thizy@emse.fr>
2018-04-24 12:10:11 +0200

    ajout relations doctrine

--- src/Emse/PlateformBundle/Controller/AdvertController.php
+++ src/Emse/PlateformBundle/Controller/AdvertController.php
@@ -2,6 +2,9 @@
 
 namespace Emse\PlateformBundle\Controller;
 
+use Emse\PlateformBundle\Entity\Advert;
+use Emse\PlateformBundle\Entity\Application;
+use Emse\PlateformBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,7 +26,9 @@ class AdvertController extends Controller
 //        if ($page < 1){
 //            throw new notFoundHttpException('page ' . $page . ' not found');
 //        }
-        return $this->render('@EmsePlateform/Advert/index.html.twig', compact('page'));
+        $em = $this->getDoctrine()->getManager();
+        $adverts = $em->getRepository('Emse\PlateformBundle\Entity\Advert')->findAll();
+        return $this->render('@EmsePlateform/Advert/index.html.twig', compact('page', 'adverts'));
     }
 
     public function menuAction($limit) {
@@ -43,25 +48,72 @@ class AdvertController extends Controller
 
 
     public function viewAction($id, Request $request) {
-        $session = $request->getSession();
-        $userId = $session->set('user_id', 19);
+        $em = $this->getDoctrine()->getManager();
+        $advert = $em->getRepository('Emse\PlateformBundle\Entity\Advert')->find($id);
 
-        return $this->render('@EmsePlateform/Advert/view.html.twig', compact('id'));
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        $listApplications = $em->getRepository('Emse\PlateformBundle\Entity\Application')->findBy(array('advert' => $advert));
+
+        return $this->render('@EmsePlateform/Advert/view.html.twig', array(
+
+            'advert' => $advert,
+            'listApplications' => $listApplications
+
+        ));
+//        $session = $request->getSession();
+//        $userId = $session->set('user_id', 19);
+//
+//        return $this->render('@EmsePlateform/Advert/view.html.twig', compact('id'));
 
     }
 
     public function addAction(Request $request) {
 
-//        if ($request->isMethod('POST')) {
-//            $session = $request->getSession();
-//            $session->getFlashbag()
-//                    ->add('info', 'ok ok ok');
-//
-//            return $this->redirectToRoute('advert_view', array('id' => 5));
-//        }
 
-        return $this->render('@EmsePlateform/Advert/add.html.twig');
+        $advert = new Advert();
+        $advert->setTitle('Recherche développeur Symfony.');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
+
+        $image = new Image();
+        $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+        $image->setAlt('Job de rêve');
+        $advert->setImage($image);
+
+// Création d'une première candidature
+
+        $application1 = new Application();
+        $application1->setAuthor('Marine');
+        $application1->setContent("J'ai toutes les qualités requises.");
+
+        $application2 = new Application();
+        $application2->setAuthor('Pierre');
+        $application2->setContent("Je suis très motivé.");
+
+        $application1->setAdvert($advert);
+        $application2->setAdvert($advert);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($advert);
+        $em->persist($application1);
+        $em->persist($application2);
+
+        $em->flush();
+
+
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+            return $this->redirectToRoute('advert_view', array('id' => $advert->getId()));
+        }
+
+        // Si on n'est pas en POST, alors on affiche le formulaire
+        return $this->render('@EmsePlateform/Advert/add.html.twig', array('advert' => $advert));
+
     }
+
 
     public function editAction(Request $request) {
 
@@ -81,12 +133,24 @@ class AdvertController extends Controller
         return $this->render('@EmsePlateform/Advert/delete.html.twig');
     }
 
-//    public function viewSlugAction($year, $slug, $format) {
-//
-//
-//        return new Response('l\'année : ' . $year . '</br> le slug : ' . $slug . '</br> format : ' . $format);
-//    }
 
+    public function editImageAction($advertId) {
+        $em = $this->getDoctrine()
+                   ->getManager();
+
+        $advert = $em->getRepository('Emse\PlateformBundle\Entity\Advert')
+                     ->find($advertId);
+        // On modifie l'URL de l'image par exemple
+        $advert->getImage()
+               ->setUrl('test.png');
+        // On n'a pas besoin de persister l'annonce ni l'image.
+        // Rappelez-vous, ces entités sont automatiquement persistées car
+        // on les a récupérées depuis Doctrine lui-même
+        // On déclenche la modification
+        $em->flush();
+
+        return new Response('OK');
+    }
 }
 
 
---
+++ src/Emse/PlateformBundle/Entity/Application.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Emse\PlateformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Application
+ *
+ * @ORM\Table(name="application")
+ * @ORM\Entity(repositoryClass="Emse\PlateformBundle\Repository\ApplicationRepository")
+ */
+class Application
+{
+    public function __construct()
+    {
+        $this->date = new \Datetime();
+    }
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+
+     * @ORM\ManyToOne(targetEntity="Emse\PlateformBundle\Entity\Advert")
+
+     * @ORM\JoinColumn(nullable=false)
+
+     */
+
+    private $advert;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set author.
+     *
+     * @param string $author
+     *
+     * @return Application
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author.
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content.
+     *
+     * @param string $content
+     *
+     * @return Application
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set date.
+     *
+     * @param \DateTime $date
+     *
+     * @return Application
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date.
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set advert.
+     *
+     * @param \Emse\PlateformBundle\Entity\Advert $advert
+     *
+     * @return Application
+     */
+    public function setAdvert(\Emse\PlateformBundle\Entity\Advert $advert)
+    {
+        $this->advert = $advert;
+
+        return $this;
+    }
+
+    /**
+     * Get advert.
+     *
+     * @return \Emse\PlateformBundle\Entity\Advert
+     */
+    public function getAdvert()
+    {
+        return $this->advert;
+    }
+}
